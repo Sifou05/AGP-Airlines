f4c89140edd2a944341d106d011309e66cf37f1c
AntoniosKotsis <antoniskotsis7@gmail.com>
2025-07-24 16:43:05 +0300

    Update ChangeBooking.java

--- src/booking/ChangeBooking.java
+++ src/booking/ChangeBooking.java
@@ -10,5 +10,18 @@ public class ChangeBooking extends JDialog
 		super(mainWindow,"Change Booking",true); 
 		this.setSize(1920, 1080);
 		this.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE); // release resources from window when we try to close this window
+
+		JMenuBar menuBar = new JMenuBar(); // menu bar
+		
+		// fields of menu bar
+		fieldReturn = new JMenu("Return");
+		fieldOneWay = new JMenu("One Way");
+		fieldMulticity = new JMenu("Multicity");
+		
+		menuBar.add(fieldReturn);
+		menuBar.add(fieldOneWay);
+		menuBar.add(fieldMulticity);
+		
+		this.setJMenuBar(menuBar);
 	}
 }
